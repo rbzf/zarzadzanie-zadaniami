ec012f3008e32713dbac765255c58ca88f351b96
rbzf <anna.sadkowska@gmail.com>
2023-07-28 22:00:52 +0200

    Utworzenie projektu

---
+++ src/main/java/pl/as/zarzadzaniezadaniami/ZarzadzanieZadaniamiApplication.java
@@ -0,0 +1,13 @@
+package pl.as.zarzadzaniezadaniami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarzadzanieZadaniamiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZarzadzanieZadaniamiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/as/zarzadzaniezadaniami/ZarzadzanieZadaniamiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.as.zarzadzaniezadaniami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarzadzanieZadaniamiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
